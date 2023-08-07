4e1bb268ed4bfb2641169606c5c4eb45815c95e9
VerdiZone <verdizone@gmail.com>
2023-08-07 09:31:18 -0600

    Primer Commit

---
+++ js/booleans.js
@@ -0,0 +1,15 @@
+//Valores Booleanos TRUE FALSE
+
+//console.log('Booleans')
+
+
+let user = true;
+let username = "Changuito"
+
+if (user) {
+    console.log(`Hola ${username}`);
+}else{
+    console.log("Hola... ¿quien eres?")
+}
+
+
---
+++ js/objetos.js
@@ -0,0 +1,49 @@
+
+//console.log('Objetos')
+
+const producto = {
+    nombre: 'pera',
+    precio: 5,
+    disponible: true,
+}
+
+const heroe1 = {
+    nombre: 'SpiderMan',
+    alterEgo: 'Peter Parker',
+    superPoder: 'Trepar paredes',
+    altura: 1.80,
+    peso: 100,
+    direccion: 'Cale Fulana 347',
+    contacto: 'Tía May',
+
+}
+
+const heroe2 = {
+    nombre: 'IronMan',
+    alterEgo: 'Tony Stark',
+    superPoder: 'Traje',
+    altura: 1.83,
+    peso: 190,
+    direccion: 'California',
+    contacto: 'Fury',
+
+}
+
+const heroe3 = {
+    nombre: 'Hulk',
+    alterEgo: 'Bruce Banner',
+    superPoder: 'Fuerza',
+    altura: 2.89,
+    peso: 450,
+    direccion: 'Desconocida',
+    contacto: 'Ninguno',
+
+}
+
+//PARA ACCEDER A LAS PROPIEDADES DE UN OBJETO SE USA UN PUNTO
+console.log(producto.precio)
+
+console.log(heroe1.superPoder);
+console.log(heroe3.nombre);
+
+
